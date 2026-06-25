66e2691df144186796a6d8c2f9b8138bd86c672f
rajesh <rajesh@gmail.com>
2026-06-25 12:58:21 +0530

    Deleted Error Fixing file

--- Backend/app/services/Error_fixing_pipelin/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Error Fixing Pipeline Package
-"""
-
-from .complete_error_fixing_pipeline import CompleteErrorFixingPipeline
-from .ui_integration import update_embeddings_from_ui
-
-__all__ = ['CompleteErrorFixingPipeline', 'update_embeddings_from_ui']
-
